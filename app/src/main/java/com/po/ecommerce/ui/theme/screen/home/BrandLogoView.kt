b6c4c6b0635984feb9c718ab098bbd50d86d39e5
shinethyuzan <shinethyuzan.jg@gmail.com>
2024-09-08 11:39:25 +0700

    delete unnecessary file

--- app/src/main/java/com/po/ecommerce/ui/theme/screen/home/BrandLogoView.kt
+++ app/src/main/java/com/po/ecommerce/ui/theme/screen/home/BrandLogoView.kt
@@ -4,24 +4,38 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.po.ecommerce.R
@@ -101,4 +115,128 @@ fun PopularHeader() {
         )
     }
 }
+
+
+@Composable
+fun SizeView(size : List<Int>) {
+    // LazyRow for horizontal scrolling of brand logo cards
+    LazyRow(
+        modifier = Modifier
+            .padding(start = 16.dp)
+            .padding(end = 16.dp)
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp) // Space between cards
+    ) {
+        items(size) { logo ->
+            SizeCard(logo.toString())
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SizeCard(logoResId: String) {
+    // Create a state to hold the background color
+    var backgroundColor by remember { mutableStateOf(Color.LightGray) }
+
+    // Card with rounded corners displaying the logo
+    Card(
+        shape = RoundedCornerShape(10.dp),
+
+        ) {
+        Box(
+            modifier = Modifier
+                .height(60.dp)
+                .size(60.dp)
+                .background(backgroundColor) // Apply the dynamic background color
+                .clickable {
+                    // Change background color on click
+                    backgroundColor =
+                        if (backgroundColor == Color.LightGray) Color(0xFFFF9800) else Color.LightGray
+                }, // Fills the entire Card
+            contentAlignment = Alignment.Center // Centers content both vertically and horizontally
+        ) {
+            Text(
+                text = logoResId,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
+@Composable
+fun ColorSelector() {
+    // List of colors to display
+    val colors = listOf(Color.White, Color.Black, Color(0xFF8BC34A), Color(0xFF7E57C2))
+
+    // Mutable state to hold selected color index
+    var selectedColorIndex by remember { mutableStateOf(0) }
+
+    Row(
+        modifier = Modifier.padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        colors.forEachIndexed { index, color ->
+            Box(
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .background(color)
+                    .clickable { selectedColorIndex = index },
+                contentAlignment = Alignment.Center
+            ) {
+                // Show checkmark only for the selected color
+                if (index == selectedColorIndex) {
+                    Icon(
+                        imageVector = Icons.Default.Check,
+                        contentDescription = "Selected",
+                        tint = Color(0xFFFF9800) // Orange color for the checkmark
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun CounterView() {
+    var count by remember { mutableStateOf(1) }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        IconButton(
+            onClick = { if (count > 0) count-- },
+            modifier = Modifier
+                .size(MaterialTheme.dimen.base_3x)
+                .background(Color.LightGray, CircleShape)
+        ) {
+            Icon(
+                modifier = Modifier
+                    .size(MaterialTheme.dimen.base_3x),
+                painter = painterResource(id = R.drawable.ic_remove),
+                contentDescription = "back",
+            )
+        }
+
+        Text(text = count.toString(), fontSize = 18.sp)
+
+        IconButton(
+            onClick = { count++ },
+            modifier = Modifier
+                .size(MaterialTheme.dimen.base_3x)
+                .background(Color.LightGray, CircleShape)
+        ) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "Increase")
+        }
+    }
+}
+
+
 
--- app/src/main/java/com/po/ecommerce/ui/theme/screen/news/DetailScreen.kt
+++ app/src/main/java/com/po/ecommerce/ui/theme/screen/news/DetailScreen.kt
@@ -11,7 +11,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -21,6 +26,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -29,12 +37,19 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.po.ecommerce.R
+import com.po.ecommerce.ui.theme.common.VerticalSpacerBase2x
+import com.po.ecommerce.ui.theme.common.VerticalSpacerBase3x
+import com.po.ecommerce.ui.theme.graph.Routes
 import com.po.ecommerce.ui.theme.resources.dimen
+import com.po.ecommerce.ui.theme.screen.home.ColorSelector
+import com.po.ecommerce.ui.theme.screen.home.CounterView
+import com.po.ecommerce.ui.theme.screen.home.SizeView
 import com.po.ecommerce.ui.theme.viewmodel.EcommerceInfoViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -44,6 +59,8 @@ fun DetailScreen(
     viewModel: EcommerceInfoViewModel = hiltViewModel()
 ) {
     val ecommerce by viewModel.selectedEcommerce.collectAsState()
+    var descriptionTextVisible by remember { mutableStateOf(false) }
+    var deliveryTextVisible by remember { mutableStateOf(false) }
 
     Scaffold(
         modifier = Modifier,
@@ -99,7 +116,12 @@ fun DetailScreen(
                 .fillMaxWidth()
                 .padding(it)
         ) {
-            Column(modifier = Modifier.padding(all = 8.dp)) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()) // Adding scroll behavior
+                    .padding(it)
+            ) {
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -152,7 +174,7 @@ fun DetailScreen(
                         style = MaterialTheme.typography.titleMedium
                     )
                 }
-
+                VerticalSpacerBase2x()
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -170,6 +192,129 @@ fun DetailScreen(
                         style = MaterialTheme.typography.titleMedium
                     )
                 }
+                Box(
+                    Modifier
+                        .fillMaxWidth()
+                ) {
+                    SizeView(ecommerce?.size ?: emptyList())
+                }
+                VerticalSpacerBase2x()
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                        .clickable {
+                            descriptionTextVisible = !descriptionTextVisible
+                        },
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    // Conditionally show or hide the Text view based on isTextVisible state
+                    Text(
+                        text = "Description",
+                        color = Color.Black,
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    Icon(
+                        modifier = Modifier
+                            .size(MaterialTheme.dimen.base_3x)
+                            .clickable {
+                                // Toggle the visibility of the Text view
+                                descriptionTextVisible = !descriptionTextVisible
+                                // Pop the back stack when the icon is clicked
+                            },
+                        painter = painterResource(id = R.drawable.ic_down_arrow),
+                        contentDescription = "back",
+                    )
+                }
+                if (descriptionTextVisible) {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp, vertical = 8.dp),
+                        text = ecommerce?.description ?: "",
+                        color = Color.Black,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+                VerticalSpacerBase2x()
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                        .clickable {
+                            deliveryTextVisible = !deliveryTextVisible
+                        },
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    // Conditionally show or hide the Text view based on isTextVisible state
+                    Text(
+                        text = "Free Delivery and Returns",
+                        color = Color.Black,
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    Icon(
+                        modifier = Modifier
+                            .size(MaterialTheme.dimen.base_3x)
+                            .clickable {
+                                // Toggle the visibility of the Text view
+                                deliveryTextVisible = !deliveryTextVisible
+                                // Pop the back stack when the icon is clicked
+                            },
+                        painter = painterResource(id = R.drawable.ic_down_arrow),
+                        contentDescription = "back",
+                    )
+                }
+                if (deliveryTextVisible) {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp, vertical = 8.dp),
+                        text = ecommerce?.deli_return ?: "",
+                        color = Color.Black,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+                VerticalSpacerBase2x()
+                Box(
+                    Modifier
+                        .fillMaxWidth()
+                ) {
+                    ColorSelector()
+                }
+                VerticalSpacerBase2x()
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                        .clickable {
+
+                        },
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    // Conditionally show or hide the Text view based on isTextVisible state
+                    Text(
+                        modifier = Modifier
+                            .padding( vertical = 8.dp),
+                        text = "Quantity",
+                        color = Color.Black,
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    CounterView()
+                }
+                VerticalSpacerBase2x()
+                Button(
+                    onClick = { navController.navigate(Routes.HOME) },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFE65100)),
+                    modifier = Modifier
+                        .padding(horizontal = MaterialTheme.dimen.base_2x)
+                        .fillMaxWidth()
+                        .height(50.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                ) {
+                    Text(text = "Add to Cart", color = Color.White, fontSize = 18.sp)
+                }
+
+                VerticalSpacerBase3x()
 
             }
         }
